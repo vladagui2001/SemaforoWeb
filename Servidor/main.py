55a678a8737c546f36a920b1a160bf22ddab9916
vladagui2001 <vlad.aguiluz5c@gmail.com>
2023-04-18 09:31:24 -0500

    modified:   Servidor/main.py

--- Servidor/main.py
+++ Servidor/main.py
@@ -2,8 +2,30 @@ from flask import Flask ,request
 from flask import jsonify 
 from index import index
 
+from flask_jwt_extended import create_access_token , get_jwt_identity
+from flask_jwt_extended import jwt_required ,JWTManager
+
+
+
 app= Flask(__name__)
 varIndex=index()
+
+app.config["JWT_SECRET_KEY"] = "SemaforoMagico"  # Change this!
+jwt = JWTManager(app)
+
+#Login cewado
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    response =  varIndex.DtoUsuario().select()
+    for item in response:
+        print(item)
+        if item["NOMBRE"] == username or item["PASSWORD"]==password:
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token)
+            #return "ok"
+    return jsonify({"msg":"por favor intrudse bien la inaforamcion requerida "})
 
 #Get Usuarios
 @app.route("/Usarios"  )
