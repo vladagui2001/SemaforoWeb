ce9be4fc1736b3114a519429caedf33cf3f2c807
vladagui2001 <vlad.aguiluz5c@gmail.com>
2023-04-26 10:46:08 -0500

    new file:   Cliente/Front_Prueba/axios.min.js 	new file:   Cliente/Front_Prueba/bootstrap.bundle.min.js 	new file:   Cliente/Front_Prueba/bootstrap.min.css 	new file:   Cliente/Front_Prueba/login.html 	new file:   Cliente/Front_Prueba/vue.global.js 	modified:   Servidor/main.py 	new file:   front/Fechas.html 	new file:   front/Registrate.html 	new file:   front/Resultados.html 	new file:   front/axios.min.js 	new file:   front/bootstrap.bundle.min.js 	new file:   front/bootstrap.min.css 	new file:   front/front.zip 	new file:   front/index.html 	new file:   front/login.html 	new file:   front/vue.global.js

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,6 +1,7 @@
 from flask import Flask ,request
 from flask import jsonify 
 from index import index
+from flask_cors import CORS
 
 from flask_jwt_extended import create_access_token , get_jwt_identity
 from flask_jwt_extended import jwt_required ,JWTManager
@@ -8,6 +9,7 @@ from flask_jwt_extended import jwt_required ,JWTManager
 
 
 app= Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 varIndex=index()
 
 app.config["JWT_SECRET_KEY"] = "SemaforoMagico"  # Change this!
@@ -16,8 +18,10 @@ jwt = JWTManager(app)
 #Login cewado
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
+    username = request.json.get("username")
+    password = request.json.get("password")
+    print(username)
+    print(password)
     response =  varIndex.DtoUsuario().select()
     for item in response:
         if item["NOMBRE"] == username or item["PASSWORD"]==password:
@@ -28,17 +32,17 @@ def login():
 
 #Get Usuarios
 @app.route("/Usarios"  )
-@jwt_required()
+#@jwt_required()
 def GetUsario():
-    current_user = get_jwt_identity()
-    print(current_user)
+    #current_user = get_jwt_identity()
+    #print(current_user)
     response =  varIndex.DtoUsuario().select()
     return jsonify(response)
 
-@app.route("/Usarios/Add", methods =["POST"] )
+"""@app.route("/Usarios/Add", methods =["POST"] )
 def AddUsuarios():
     if request.method == 'POST':
-        return ("OK")
+        return ("OK")"""
 
 #Get Semáforos
 @app.route("/Semaforos"  )
@@ -46,10 +50,10 @@ def GetSemaforo():
     response =  varIndex.DtoSemaforos().select()
     return jsonify(response)
 
-@app.route("/Semaforos/Add", methods =["POST"] )
+"""@app.route("/Semaforos/Add", methods =["POST"] )
 def AddUSemaforo():
     if request.method == 'POST':
-        return ("OK")  
+        return ("OK")  """
 
 
 
