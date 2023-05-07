63afb4ef776f7c3df125d4de3abedc40728925e0
vladagui2001 <vlad.aguiluz5c@gmail.com>
2023-05-06 19:08:06 -0500

    modified:   Cliente/front/src/views/HomeView.vue

--- Cliente/front/src/views/HomeView.vue
+++ Cliente/front/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div>
-      <p>Por favor inicia Sesión si no lo has hecho <router-link class="nav-link" to="/login">Iniciar Sesión</router-link></p>
+      <p>Por favor inicia secion si no lo as echo <router-link class="nav-link" to="/login">Inisar Secion</router-link></p>
     </div>
     <div>
-      <p>Gracias por Iniciar Sesión.</p>
+      <p>Gracias por iniciar secion</p>
     </div>
   </div>
 </template>
